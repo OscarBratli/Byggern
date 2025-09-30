b740ba758aec00514a6b5405d90cb7a0f50c61a6
OscarBratli <racsodimmen@gmail.com>
2025-09-30 15:50:56 +0200

    UART config change

--- src/uart/uart.h
+++ src/uart/uart.h
@@ -7,8 +7,11 @@
 #include <avr/interrupt.h>
 
 // === UART config ===
-#define BAUD 9600
-#define MYUBRR (F_CPU / 16 / BAUD - 1)
+#define F_CPU 4915200UL  // 4.9152 MHz crystal atmega162
+#define BAUD 9600       // 9600 baud rate 
+#define MYUBRR (F_CPU / 16 / BAUD - 1) //Calculate MYUBRR value for given F_CPU and BAUD rate should be 31 for 4.9152MHz and 9600 baud rate
+
+
 
 // === API ===
 void uart_init(unsigned int ubrr);
