8bf2bf86095042b257552bf070e4c6b985c56cf6
OscarBratli <racsodimmen@gmail.com>
2025-10-01 20:13:32 +0200

    Remove double definition of F_CPU

--- src/uart/uart.h
+++ src/uart/uart.h
@@ -7,7 +7,6 @@
 #include <avr/interrupt.h>
 
 // === UART config ===
-#define F_CPU 4915200UL  // 4.9152 MHz crystal atmega162
 #define BAUD 9600       // 9600 baud rate 
 #define MYUBRR (F_CPU / 16 / BAUD - 1) //Calculate MYUBRR value for given F_CPU and BAUD rate should be 31 for 4.9152MHz and 9600 baud rate
 
